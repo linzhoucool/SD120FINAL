5d70909158c6ad16a1d077ae887abf07074cc4eb
Lin <1159148810@qq.com>
2018-05-16 17:27:51 -0500

    i use the gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 let cleanCSS = require('gulp-clean-css');
-
+const imagemin = require('gulp-imagemin');
 
 gulp.task('CSS', () =>
     gulp.src('./src/css/*css')
@@ -13,3 +13,8 @@ gulp.task('CSS', () =>
         .pipe(gulp.dest('dist/css'))
 );
 
+gulp.task('IMAGES', () =>
+    gulp.src('./src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
